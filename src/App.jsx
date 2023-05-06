b6053aee95f39d46d9f2b7c2820519edf038b115
CrosscutX <crosscutcode@gmail.com>
2023-05-06 18:50:46 -0400

    Start to memory fighter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Memory Fighter</h1>
+        <div className="score">
+          <span>Score: 0</span>
+          <span>Best Score: 0</span>
+        </div>
+      </header>
+    </div>
+  );
+}
